c88fe0b6b492f2452e01b255f1a7b0c4437e6f38
Elijah De Calmer <elijahdecalmer@gmail.com>
2024-08-25 13:36:07 +1000

    Initia commit. Login mockup and sidebar mockup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    LoginComponent,
+    HomeComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], // Corrected from styleUrl to styleUrls
 })
 export class AppComponent {
   title = 'PortalChat';
+  isSidebarOpen = false;
+
+  constructor(private router: Router) {}
+
+  openSidebar() {
+    this.isSidebarOpen = true;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
